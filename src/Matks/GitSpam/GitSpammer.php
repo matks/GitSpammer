a6c41fcee9f564a463beedf2b2511eb5aaf548e2
Mathieu Ferment <mathieu.ferment@mobpartner.com>
2014-10-07 22:30:35 +0200

    First GitSpammer files

---
+++ src/Matks/GitSpam/GitSpammer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Matks\GitSpam;
+
+class GitSpammer
+{
+    /**
+	 * Constructor
+	 */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute GitSpam
+     */
+    public function run()
+    {
+    }
+}
